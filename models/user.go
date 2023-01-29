26eb5b05e45eb2e7f9362e300402f15db5f33cf5
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2023-01-29 09:11:30 +0100

    created a level of abstraction for the methods

--- models/user.go
+++ models/user.go
@@ -1,15 +1,80 @@
 package models
 
 import (
-	"golang.org/x/crypto/bcrypt"
-	"github.com/go-redis/redis"
 	"errors"
+	"fmt"
+
+	"github.com/go-redis/redis"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
-	ErrNotFound = errors.New("user not found")
+	ErrNotFound     = errors.New("user not found")
 	ErrInvalidLogin = errors.New("Invalid login")
 )
+
+type User struct {
+	key string
+}
+
+func NewUser(username string, hash []byte) (*User, error) {
+	id, err := client.Incr("user:next-id").Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	key := fmt.Sprintf("user:%d", id)
+
+	pipe := client.Pipeline()
+	pipe.HSet(key, "id", id)
+	pipe.HSet(key, "username", username)
+	pipe.HSet(key, "hash", hash)
+	pipe.HSet("user:by-username", username, id)
+	_, err = pipe.Exec()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{key}, nil
+}
+
+func (user *User) GetUsername() (string, error) {
+	return client.HGet(user.key, "username").Result()
+}
+
+func (user *User) GetHash() ([]byte, error) {
+	return client.HGet(user.key, "hash").Bytes()
+}
+
+func (user *User) Authenticate(password string) error {
+	hash, err := user.GetHash()
+
+	if err != nil {
+		return err
+	}
+
+	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return ErrInvalidLogin
+	}
+
+	return err
+}
+
+func GetUserByUsername(username string) (*User, error) {
+	id, err := client.HGet("user:by-username", username).Int64()
+	if err == redis.Nil {
+		return nil, ErrNotFound
+	} else if err != nil {
+		return nil, err
+	}
+
+	key := fmt.Sprintf("user:%d", id)
+	return &User{key}, nil
+}
 
 func RegisterUser(username, password string) error {
 	cost := bcrypt.DefaultCost
@@ -19,26 +84,16 @@ func RegisterUser(username, password string) error {
 		return err
 	}
 
-	return client.Set("user:"+username, hash, 0).Err()
-
-
+	_, err = NewUser(username, hash)
+	return err
 }
 
-
 func AuthenticateUser(username, password string) error {
-	hash, err := client.Get("user:" + username).Bytes()
 
-	if err == redis.Nil {
-		return ErrNotFound
-	} else if err != nil {
+	user, err := GetUserByUsername(username)
+	if err != nil {
 		return err
 	}
 
-	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
-
-	if err != nil {
-		return ErrInvalidLogin
-	}
-
-	return nil
+	return user.Authenticate(password)
 }
