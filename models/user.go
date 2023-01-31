49255dbad634b699a76a306e5ae7625093dd16f9
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2023-01-31 15:18:43 +0100

    changed the key

--- models/user.go
+++ models/user.go
@@ -14,7 +14,7 @@ var (
 )
 
 type User struct {
-	key string
+	id int64
 }
 
 func NewUser(username string, hash []byte) (*User, error) {
@@ -37,19 +37,21 @@ func NewUser(username string, hash []byte) (*User, error) {
 		return nil, err
 	}
 
-	return &User{key}, nil
+	return &User{id}, nil
 }
 
 func (user *User) GetId() (int64, error) {
-	return client.HGet(user.key, "id").Int64()
+	return user.id, nil
 }
 
 func (user *User) GetUsername() (string, error) {
-	return client.HGet(user.key, "username").Result()
+	key := fmt.Sprintf("user:%d", user.id)
+	return client.HGet(key, "username").Result()
 }
 
 func (user *User) GetHash() ([]byte, error) {
-	return client.HGet(user.key, "hash").Bytes()
+	key := fmt.Sprintf("user:%d", user.id)
+	return client.HGet(key, "hash").Bytes()
 }
 
 func (user *User) Authenticate(password string) error {
@@ -69,8 +71,8 @@ func (user *User) Authenticate(password string) error {
 }
 
 func GetUserById(id int64) (*User, error) {
-	key := fmt.Sprintf("user:%d", id)
-	return &User{key}, nil
+	
+	return &User{id }, nil
 }
 
 func GetUserByUsername(username string) (*User, error) {
